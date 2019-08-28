296e9e9fc745dcd1740b8a2ff6ad391d1e6fbdc6
MelanieJaneBond <miss.bond2@gmail.com>
2019-08-28 09:10:01 -0500

    added file structure

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import { BrowserRouter as Router } from "react-router-dom"
+// import './index.css';
+// import ChoresMain from './components/ChoresMain';
+
+// ReactDOM.render(
+//     <Router>
+//         <ChoresMain />
+//     </Router>
+//     , document.getElementById('root'));
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
